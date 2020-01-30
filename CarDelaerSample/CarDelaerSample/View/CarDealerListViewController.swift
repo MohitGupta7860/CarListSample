b48ec2b6f3a57a2e2cf8d5025d13384139eb27ef
Mohit <mohit.gupta7860@gmail.com>
2020-01-29 22:43:59 -0500

    Initial setup of Project

---
+++ CarDelaerSample/CarDelaerSample/View/CarDealerListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CarDelaerSample
+//
+//  Created by Mohit on 29/01/20.
+//  Copyright © 2020 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class CarDealerListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
